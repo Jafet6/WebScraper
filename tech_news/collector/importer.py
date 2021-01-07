48c3ff06ba177b7bb0b7d2ad2eb259799a16b742
Jafet Henrique Guerra Fagundes <jafetguerra6@gmail.com>
2021-01-07 12:12:31 -0300

    Add files via upload

---
+++ tech_news/collector/importer.py
@@ -0,0 +1,21 @@
+import csv
+from os import path
+
+
+def csv_importer(filepath):
+    exists = path.exists(filepath)
+    if not filepath.endswith('.csv'):
+        raise ValueError('Formato invalido')
+    if not exists:
+        raise ValueError('Arquivo file_not_exist.csv não encontrado')
+
+    data_list = []
+    with open(filepath) as file:
+        readable = csv.reader(file, delimiter=";", quotechar='"')
+        headers, *data = readable
+        for list in data:
+            dict = {}
+            for index, header in enumerate(headers):
+                dict[header] = list[index]
+            data_list.append(dict)
+    return data_list
